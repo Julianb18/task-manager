eb63349cdaf46acf6fd83800f7ac4ba46f8f7498
Julianb18 <julian141718@gmail.com>
2020-12-05 15:03:22 +0100

    add task and remove task function

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import "./App.css";
 import { TaskList } from "./components/TaskList";
 import TaskListContextProvider from "./context/TaskListContext";
 import { TaskForm } from "./components/TaskForm";
+import { Header } from "./components/Header";
 
 function App() {
   return (
     <TaskListContextProvider>
       <div className="container">
         <div className="app-wrapper">
+          <Header />
           <div className="main">
             <TaskForm />
             <TaskList />
